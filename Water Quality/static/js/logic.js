8aea0441adc04ad08c9754a7f696ed2419d639bb
Haley Huhtala <haley.huhtala@gmail.com>
2020-07-16 11:16:39 -0500

    adding cluster markers, still need to add pop ups

--- Water Quality/static/js/logic.js
+++ Water Quality/static/js/logic.js
@@ -1,18 +1,31 @@
 // Creating map object
-var myMap = L.map("map", {
+var map = L.map("map", {
   center: [46.7, -94.7],
   zoom: 7
 });
 
+// Initialize LayerGroups
+var layers = {
+  water_layer: new L.LayerGroup(),
+  air_layer: new L.LayerGroup()
+};
+
 // Adding tile layer to the map
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+water_layer = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
   maxZoom: 18,
   zoomOffset: -1,
   id: "mapbox/streets-v11",
   accessToken: API_KEY
-}).addTo(myMap);
+})
+
+water_layer.addTo(map);
+
+// initalize
+var locationSet = new Set();
+var water_markers = L.markerClusterGroup();
+var water_marker_list = [];
 
 // Store API query variables
 var baseURL = "https://enviro.epa.gov/enviro/efservice/";
@@ -30,7 +43,7 @@ let operator2 = "!=/";
 let value = "null/";
 let operator = "CONTAINING/"
 let operator3 = "BEGINNING/"
-let date =  "-18/"; // "01-JAN-18/";
+let date = "-18/"; // "01-JAN-18/";
 let activity = "pws_activity_code/";
 let code = "A/";
 let other = "rows/1:19/"
@@ -40,51 +53,67 @@ let count = "count"
 //https://enviro.epa.gov/enviro/efservice/violation/compl_per_begin_date/CONTAINING/-18/rows/1:19/JSON
 
 // Assemble API query URL
-var url1 = baseURL + table + column + state + table2 + other + type; //basic H2O sys + violations
-var url2 = baseURL + table + column + state + table2 + other + type; //H2O sys and Violation date *columns diff than expected + column2 + operator + date
-var url3 = baseURL + table + column + state + table2 + column3 + yes + other + type; //H2O sys and violation not null *still contained nulls
-var url4 = baseURL + table + column + state + table2 + table2 + value + other + type; //violation null doesn't work
-var url5 = baseURL + table2 + column2 + operator + date + other + type; //violation date
-var url6 = baseURL + table + column + state + table2 + column3 + yes + other + type; //
-var url7 = baseURL + table + column + state + table2 + column2 + operator + date + other + type; // returns violations that are null
-var url8 = baseURL + table + column + state + activity + code + table2 + count; //active H2O sys + violations 60005
-var url9 = baseURL + table + column + state + column4 + operator + yes + count; //active H2O sys + outstanding performer *non found
-var url10 = baseURL + table2 + column5 + operator3 + state + column2 + operator + date + table + other + type; //MN 18 violations 343 
-var url11 = baseURL + table2 + column5 + operator3 + state + column2 + operator + date + column3 + yes + table + other + type; //MN 18 violations health 80
-
+var url11 = baseURL + table2 + column5 + operator3 + state + column2 + operator + date + column3 + yes + table + type; //MN 18 violations health 80
 
 console.log(url11);
 
-// Grab the data with d3
-d3.json(url11, function(response) {
-  console.log(response[0]);
-  console.log(response[0].WATER_SYSTEM);
-  console.log(response[0].WATER_SYSTEM.WATER_SYSTEM_ROW);
-  console.log(response[0].WATER_SYSTEM.WATER_SYSTEM_ROW.PWSID);
+// Get water violation data, geocode to find lat long, create markers
+d3.json(url11, function (response) {
+  // console.log(response[0]);
 
-  // // Create a new marker cluster group
-  // var markers = L.markerClusterGroup();
+  response.forEach(function (f) {
+  
+    let address1 = f.WATER_SYSTEM.WATER_SYSTEM_ROW.ADDRESS_LINE1;
+    let address2 = f.WATER_SYSTEM.WATER_SYSTEM_ROW.ADDRESS_LINE2;
+    let city = f.WATER_SYSTEM.WATER_SYSTEM_ROW.CITY_NAME;
+    let state = f.WATER_SYSTEM.WATER_SYSTEM_ROW.STATE_CODE;
+    let locationName = `${address1}, ${address2}, ${city}, ${state}`;
+    //geocode to find lat long
+    sendGeocodingRequest(locationName)
+      .then(function (data) {
+        // console.log(data)
+        //and if it is successful create marker
+        createMarker(data)
+      })
 
-  // // Loop through data
-  // response.forEach(function(response) {
-  //   console.log(response.features[0].properties.mag);
-  // });
-  // for (var i = 0; i < response.length; i++) {
+    // locationSet.add(locationName);
+    // console.log(locationSet);
+  });
 
-  //   // Set the data location property to a variable
-  //   var location = response[i].location;
-
-  //   // Check for location property
-  //   if (location) {
-
-  //     // Add a new marker to the cluster group and bind a pop-up
-  //     markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
-  //       .bindPopup(response[i].descriptor));
-  //   }
-
-  // }
-
-  // // Add our marker cluster layer to the map
-  // myMap.addLayer(markers);
+  console.log(water_markers);
+  map.addLayer(water_markers);
 
 });
+
+// Create an overlays object to add to the layer control
+var overlays = {
+  "Water Quality Violations": layers.water_layer
+  // ,"Earthquakes": layers.earthquakes_layer
+};
+
+// Create a control for our layers, add our overlay layers to it
+L.control.layers(null, overlays).addTo(map);
+
+function sendGeocodingRequest(location) {
+
+  var headers = {
+    "X-Application-Id": APPLICATION_ID,
+    "X-Api-Key": GC_API_KEY,
+    // "Content-Type": "application/json",
+    "Accept-Language": "en-US"
+  };
+  return fetch(`https://api.traveltimeapp.com/v4/geocoding/search?query=` + location, {
+    method: "GET",
+    credentials: "same-origin",
+    "headers": headers
+  })
+    .then(response => response.json()); // parses JSON response into native Javascript objects
+};
+
+function createMarker(response) {  // We need to extract the coordinates from the response.
+
+  var coordinates = response.features[0].geometry.coordinates;  // The coordintaes are in a [<lng>, <lat>] format/
+  var latLng = L.latLng([coordinates[1], coordinates[0]]);
+  water_markers.addLayer(L.marker(latLng));
+
+};
